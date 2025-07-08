2d1a4e668e2fdd2c266566291a93114340ecaab4
Madhusha Laksitha <madhusha.sum@gmail.com>
2025-07-08 22:40:47 +0530

    remove all code in services and added new discovery server and api gateway

--- services/course-management-service/src/main/java/com/academix/course/model/CourseStatus.java
+++
@@ -1,8 +0,0 @@
-package com.academix.course.model;
-
-public enum CourseStatus {
-    DRAFT,
-    PUBLISHED,
-    ARCHIVED,
-    DELETED // For soft-deletion
-}
--- services/user-management-service/src/main/java/com/academix/user/service/email/EmailService.java
+++
@@ -1,5 +0,0 @@
-package com.academix.user.service.email;
-
-public interface EmailService {
-    void sendEmail(String to, String subject, String body);
-}
